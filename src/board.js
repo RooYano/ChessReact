6bd515da11d975215029a727af9a15b8945e8ba8
Ryu <ry5tq@virginia.edu>
2022-02-28 16:56:25 -0500

    working board created

--- src/board.js
+++ src/board.js
@@ -6,39 +6,86 @@ function Square(props) {
         <td
             id={props.value}
             className='square'
-            onDragOver={props.onDragOver}>
+            // onDragOver={props.onDragOver}
+            >
             {props.value}
-
         </td>
-    )
+    );
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(64).fill(null)
+        }
+    }
+
     renderSquare(i) {
         return (
             <Square 
-                value={this.props.squares[i]}
-                onDragOver={() => this.props.onDragOver(i)}
+                value={i}
+                // onDragOver
+                key={i}
             />
-        );
+        )
     }
-    
+
+    renderRow(row) {
+        var rows = [];
+        for( let i = (8 * (row - 1)); i < (row * 8); i++) {
+            rows.push(this.renderSquare(i))
+        }
+
+        return <tr><td className={'row ' + row}>{row}</td>{rows}</tr>
+
+    }
+
     render() {
         return (
             <div className='game-board'>
                 <table>
-                    <tr>
-                        <th></th>
-                        <th>A</th>
-                        <th>B</th>
-                        <th>C</th>
-                        <th>D</th>
-                        <th>E</th>
-                        <th>F</th>
-                        <th>G</th>
-                        <th>H</th>
-                    </tr>
-                    {for (let i = )}
+                    <thead>
+                        <tr>
+                            <th> </th>
+                            <th>A</th>
+                            <th>B</th>
+                            <th>C</th>
+                            <th>D</th>
+                            <th>E</th>
+                            <th>F</th>
+                            <th>G</th>
+                            <th>H</th>
+                        </tr>
+                        
+                    </thead>
+                    <tbody>
+                    {this.renderRow(8)}
+                    {this.renderRow(7)}
+                    {this.renderRow(6)}
+                    {this.renderRow(5)}
+                    {this.renderRow(4)}
+                    {this.renderRow(3)}
+                    {this.renderRow(2)}
+                    {this.renderRow(1)}
+                    </tbody>
+
+                    <tfoot>
+                        <tr>
+                            <th> </th>
+                            <th>A</th>
+                            <th>B</th>
+                            <th>C</th>
+                            <th>D</th>
+                            <th>E</th>
+                            <th>F</th>
+                            <th>G</th>
+                            <th>H</th>
+                        </tr>
+                    </tfoot>
+                        
+
+
 
                 </table>
 
@@ -46,3 +93,5 @@ class Board extends React.Component {
         )
     }
 }
+
+export default Board;
