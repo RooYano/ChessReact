1704f4ecf10bf9608086ae91f62d5b88793922a0
Ryu <ry5tq@virginia.edu>
2022-03-01 22:41:50 -0500

    fixed image file location

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+export const bB = require('./bbishop.png');
+export const kB = require('./bking.png');
+export const nB = require('./bknight.png');
+export const pB = require('./bpawn.png');
+export const qB = require('./bqueen.png');
+export const rB = require('./brook.png');
+export const bW = require('./wbishop.png');
+export const kW = require('./wking.png');
+export const nW = require('./wknight.png');
+export const pW = require('./wpawn.png');
+export const qW = require('./wqueen.png');
+export const rW = require('./wrook.png');
+
