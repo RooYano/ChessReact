6916fd42fbd1aa18d47b46202287b022c1d20a67
Ryu <ry5tq@virginia.edu>
2022-03-01 22:18:46 -0500

    adding image files. index.js created in assets image folder to export all files in one go

---
+++ src/Components/piece.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { bB, kB, nB, pB, qB, rB, bW, kW, nW, pW, qW, rW } from '../assets/images';
+
+const piece = (props) => {
+    let image = bb
+
+    return (
+       <img src={image} />
+    )
+
+}
+
+
+export default piece;
---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+export const bB = require('./images/bbishop.png');
+export const kB = require('./images/bking.png');
+export const nB = require('./images/bknight.png');
+export const pB = require('./images/bpawn.png');
+export const qB = require('./images/bqueen.png');
+export const rB = require('./images/brook.png');
+export const bW = require('./images/wbishop.png');
+export const kW = require('./images/wking.png');
+export const nW = require('./images/wknight.png');
+export const pW = require('./images/wpawn.png');
+export const qW = require('./images/wqueen.png');
+export const rW = require('./images/wrook.png');
+
